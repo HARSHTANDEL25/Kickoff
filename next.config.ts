e6085c46be2be2615fbf67412240b43c624989b2
HARSHtandel25 <harshtandel2508@gmail.com>
2026-05-21 16:28:18 +0530

    allow the remote patterns to fetch the images from apis

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,21 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images:{
+        remotePatterns: [
+      { protocol: 'https', hostname: 'media.api-sports.io' },
+      { protocol: 'https', hostname: 'e1.365dm.com' },
+      { protocol: 'https', hostname: 'e0.365dm.com' },
+      { protocol: 'https', hostname: 'e2.365dm.com' },
+      { protocol: 'https', hostname: 'ichef.bbci.co.uk' },
+      { protocol: 'https', hostname: 'i2.wp.com' },
+      { protocol: 'https', hostname: 'a.espncdn.com' },
+      { protocol: 'https', hostname: 'icdn.football-italia.net' },
+      { protocol: 'https', hostname: 'icdn.caughtoffside.com' },
+
+    ],
+  }
+  
 };
 
 export default nextConfig;
